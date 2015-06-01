6c877a951c315a89ab3968d2ec40ed44da8cd05d
TADOKORO Saneyuki <saneyan@mail.gfunction.com>
2015-06-01 18:22:44 +0900

    modify form layout

--- app/index.js
+++ app/index.js
@@ -3,14 +3,13 @@
  *
  * @author  TADOKORO Saneyuki <saneyan@seginus.jp>
  */
+
+var server;
 var fs = require('fs');
-var https = require('https');
 var express = require('express');
 var bodyParser = require('body-parser');
 var errorh = require('./error/handlers');
-var key = fs.readFileSync('./localhost.key', 'utf-8');
-var cert = fs.readFileSync('./localhost.crt', 'utf-8');
-
+var config = require('./config');
 var app = express();
 
 app.use(express.static('app/public'));
@@ -23,4 +22,13 @@ app.use(errorh.logErrors);
 app.use(errorh.badRequestErrorHandler);
 app.use(errorh.errorHandler);
 
-https.createServer({key: key, cert: cert}, app).listen(3000);
+if (config.server.ssl) {
+  server = require('https').createServer({
+    key: fs.readFileSync('./localhost.key', 'utf-8'),
+    cert: fs.readFileSync('./localhost.crt', 'utf-8')
+  }, app);
+} else {
+  server = require('http').createServer(app);
+}
+
+server.listen(config.server.port);
